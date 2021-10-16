c14cf782db060f13cddf2f46218f7a47f9fe2d6d
Alex Poon <mundanesurrealism@gmail.com>
2021-10-16 22:27:56 +0800

    Update runmel.py

--- 3dmesh/util/runmel.py
+++ 3dmesh/util/runmel.py
@@ -1,6 +1,12 @@
-import maya.cmds as cmds
-import maya.mel as mel
+import sys
+from subprocess import check_output, DEVNULL
 
-mel.eval('file -import -type "OBJ"  -ignoreVersion -ra true -mergeNamespacesOnClash false -namespace "chopchop5" -options "mo=1"  -pr  -importFrameRate true  -importTimeRange "override" "{}";')
-mel.eval('SelectAll;')
-mel.eval('if(!`exists MTselAll`) source MTprocs.mel; select -cl; MTselAll;');
+if __name__ == '__main__':
+	mel = f'''
+	import maya.cmds as cmds
+	import maya.mel as mel
+
+	mel.eval('file -import -type "OBJ"  -ignoreVersion -ra true -mergeNamespacesOnClash false -namespace "chopchop5" -options "mo=1"  -pr  -importFrameRate true  -importTimeRange "override" "{sys.argv[1]}";')
+	mel.eval('SelectAll;')
+	mel.eval('if(!`exists MTselAll`) source MTprocs.mel; select -cl; MTselAll;');
+	'''
