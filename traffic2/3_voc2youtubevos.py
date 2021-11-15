a5cd30f8eed15ff07697024fbf44ca194cc5f603
SoftFeta <mundanesurrealism@gmail.com>
2021-11-15 14:28:16 +0800

    Refactor

--- traffic2/3_voc2youtubevos.py
+++ traffic2/3_voc2youtubevos.py
@@ -1,23 +1,32 @@
 '''
 under construction
 '''
-
 from glob import glob
 from json import dumps, loads
 from os.path import basename
+from natsort import natsorted
 
 if __name__ == '__main__':
 
 	obj = {'video': {}}
 
-	for g in glob('_internal/CUH-Dataset/JPEGImages/*'):
-		# Get info from 2_
+	for g in glob('_internal/CUH-Dataset/JPEGImages/*/'):
+		od = {}
 
-		obj['video'][basename(g)] = {"objects": {}}
-		{
-	        "category": "penguin", 
-	        "frames": []
-        }
+		# Read json files
+		for h in natsorted(glob(f'{g}/*.json')):
+			print(h)
+			with open(h, 'r') as h2:
+				h3 = loads(h2.read())
+			for shape in h3['shapes']:
+				if shape['label'] in od:
+					od[shape['label']]['frames'].append(basename(h))
+				else:
+					od[shape['label']] = {'category': shape['label'].split('_')[0], 'frames': [basename(h)]}
+
+
+		obj['video'][basename(g[:-1])] = {"objects": od}
+
 
 	jsn=dumps(obj, indent=4)
 	print(jsn)
